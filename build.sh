259d1b53f46647a92fe759b3f00468e95bb6fd37
J C Lawrence <claw@kanga.nu>
2015-06-13 22:55:15 -0700

    build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+set -e
+
+lyx --export pdf2 ./1839-Rules.lyx
+mkdir -p build
+pushd build
+  mv ../1839-Rules.pdf .
+  xxpaper ../1839-Papers.xxp $*
+  for file in *.ps
+  do
+    ps2pdf $file
+  done
+  rm *.ps
+popd
