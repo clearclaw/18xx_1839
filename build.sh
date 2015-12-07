8d1152eb924c3d3e16d1fd1628b04d7438d65d71
J C Lawrence <claw@kanga.nu>
2015-12-06 19:17:05 -0800

    Token pricing

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir -p build
 pushd build
   mv ../1839-Rules.pdf ./
   xxpaper ../1839-Papers.xxp $*
-  rm market_sheet1-nooutline*
+  rm market_*-nooutline*
   cp ../1839-Papers.xxp ./
   cp ../README.txt ./
   cp ../M39.pdf ./1839-Map.pdf
