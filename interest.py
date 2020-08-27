b79aad6ac7403c8afebeec584562d712d9298f46
J C Lawrence <claw@kanga.nu>
2020-08-26 22:50:11 -0700

    Rules checkpoint

---
+++ interest.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+value = 40
+rate = 1.085
+#fix = 0.0021
+fix = 0.00305
+old = value
+
+r = rate - fix
+for i in xrange (26):
+  print "#%2d" % i, "Value: %3d" % value, "Gain: %f" % (float (value) / float (old)), "Delta: %d" % (value - old)
+  old = value
+  r += (fix if i <= 15 else - (fix * 1.64))
+  value = int ((value * r) + 0.5)
---
+++ interest2.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+value = 10
+rate = 1.27947
+#fix = 0.0021
+fix = -0.0115
+old = value
+
+r = rate - fix
+for i in xrange (26):
+  print "#%2d" % i, "Value: %3d" % value, "Gain: %f" % (float (value) / float (old)), "Delta: %d" % (value - old)
+  old = value
+  r += fix
+  value = int ((value * r) + 0.5)
