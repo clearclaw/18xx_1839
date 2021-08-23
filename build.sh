ea4c5032ecccb500752c32055d1aa932b2f0a41b
J C Lawrence <jcl@dremio.com>
2021-08-22 23:16:55 -0700

    Verb agreement

--- build.sh
+++ build.sh
@@ -1,19 +1,36 @@
 #! /bin/bash
 set -e
 
-lyx --export pdf2 ./1839-Rules.lyx
+ARG="${1}"
+GAMENAME=1839
+
+dir_root=`pwd`
+
 mkdir -p build
-pushd build
-  mv ../1839-Rules.pdf ./
-  xxpaper ../1839-Papers.xxp $*
-  \rm market_*-nooutline* || true
-  cp ../1839-Papers.xxp ./
-  cp ../README.txt ./
-  cp ../M39.pdf ./1839-Map.pdf
-  cp ../P39.pdf ./1839-Tiles.pdf
-  for file in *.ps
-  do
-    ps2pdf $file
-  done
-  rm *.ps
-popd
+rm -f build/* || true
+
+#if [[ ! -f "1828-Market.png" || ! -f "1828-Map.png" ]]
+#then
+#  echo "Making rules images"
+#  \rm -f 1828-Map.png 1828-Market.png
+#  convert -density 150 1828-Map-B2.pdf -quality 90 1828-Map.png
+#  convert -density 150 1828-Market-tabloid.pdf -quality 90 1828-Market.png
+#fi
+
+lyx --export pdf2 ./${GAMENAME}-Rules.lyx
+mv ./${GAMENAME}-Rules.pdf build
+
+cp ${GAMENAME}-Papers.xxp build/
+cp LICENSE build/
+
+for paper in letter A4
+do
+  xxpaper make ${GAMENAME}-Papers -p ${paper} build/${GAMENAME}-Papers-${paper}-diecut.pdf
+  xxpaper make ${GAMENAME}-Papers -p ${paper} -c build/${GAMENAME}-Papers-${paper}-outline.pdf
+  pdfunite masthead-${paper}.pdf box-sides-${paper}.pdf build/${GAMENAME}-BoxCovers-${paper}.pdf
+done
+for suffix in Map-A3 Map-A4 Map-B2 Map-letter Map-tabloid Market-A3 Market-A4 Market-letter \
+              Market-tabloid TrackTiles-letter-diecut TrackTiles-letter-outline
+do
+  cp ${GAMENAME}-${suffix}.pdf build
+done  
