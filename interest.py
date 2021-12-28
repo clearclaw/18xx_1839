b26122419584a68ae6c931ea28b94de3f96f8cde
J C Lawrence <jcl@dremio.com>
2021-12-27 19:48:03 -0800

    playtest release

--- interest.py
+++ interest.py
@@ -7,8 +7,8 @@ fix = 0.00305
 old = value
 
 r = rate - fix
-for i in xrange (26):
-  print "#%2d" % i, "Value: %3d" % value, "Gain: %f" % (float (value) / float (old)), "Delta: %d" % (value - old)
+for i in range (26):
+  print ("#%2d" % i, "Value: %3d" % value, "Gain: %f" % (float (value) / float (old)), "Delta: %d" % (value - old))
   old = value
   r += (fix if i <= 15 else - (fix * 1.64))
   value = int ((value * r) + 0.5)
--- interest2.py
+++ interest2.py
@@ -7,8 +7,8 @@ fix = -0.0115
 old = value
 
 r = rate - fix
-for i in xrange (26):
-  print "#%2d" % i, "Value: %3d" % value, "Gain: %f" % (float (value) / float (old)), "Delta: %d" % (value - old)
+for i in range (26):
+  print ("#%2d" % i, "Value: %3d" % value, "Gain: %f" % (float (value) / float (old)), "Delta: %d" % (value - old))
   old = value
   r += fix
   value = int ((value * r) + 0.5)
