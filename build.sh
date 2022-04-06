33c451cfd9d6585b06c7a89a5e741d3d91ba9ab6
J C Lawrence <jcl@dremio.com>
2022-04-06 13:02:42 -0700

    Throw away brown train on concession with private

--- build.sh
+++ build.sh
@@ -9,12 +9,12 @@ dir_root=`pwd`
 mkdir -p build
 rm -f build/* || true
 
-#if [[ ! -f "1828-Market.png" || ! -f "1828-Map.png" ]]
+#if [[ ! -f "${GAMENAME}-Market.png" || ! -f "${GAMENAME}-Map.png" ]]
 #then
 #  echo "Making rules images"
-#  \rm -f 1828-Map.png 1828-Market.png
-#  convert -density 150 1828-Map-B2.pdf -quality 90 1828-Map.png
-#  convert -density 150 1828-Market-tabloid.pdf -quality 90 1828-Market.png
+#  \rm -f ${GAMENAME}-Map.png ${GAMENAME}-Market.png
+#  convert -density 150 ${GAMENAME}-Map-B2.pdf -quality 90 ${GAMENAME}-Map.png
+#  convert -density 150 ${GAMENAME}-Market-tabloid.pdf -quality 90 ${GAMENAME}-Market.png
 #fi
 
 lyx --export pdf2 ./${GAMENAME}-Rules.lyx
@@ -27,9 +27,8 @@ for paper in letter A4
 do
   xxpaper make ${GAMENAME}-Papers -p ${paper} build/${GAMENAME}-Papers-${paper}-diecut.pdf
   xxpaper make ${GAMENAME}-Papers -p ${paper} -c build/${GAMENAME}-Papers-${paper}-outline.pdf
-  pdfunite masthead-${paper}.pdf box-sides-${paper}.pdf build/${GAMENAME}-BoxCovers-${paper}.pdf
 done
-for suffix in Map-A3 Map-A4 Map-B2 Map-letter Map-tabloid Market-A3 Market-A4 Market-letter \
+for suffix in Map-A4 Map-B2 Map-letter Map-tabloid Market-A4 Market-letter \
               Market-tabloid TrackTiles-letter-diecut TrackTiles-letter-outline
 do
   cp ${GAMENAME}-${suffix}.pdf build
